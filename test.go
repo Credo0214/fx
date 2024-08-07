a0a0527779cd55e6028d47f7c3cdb1c7f43c149b
Kazu0214 <ny3416944@gmail.com>
2024-08-08 02:56:57 +0900

    add

---
+++ test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	//dp float32 = 0.01
+	days int = 20
+)
+
+
+func main(){	
+	var capitall float64
+	var count int
+	var dp float64
+
+	fmt.Print("初期資金\n")
+	fmt.Scan(&capitall)
+	fmt.Print("日利\n")
+	fmt.Scan(&dp)
+	fmt.Print("何カ月\n")
+	fmt.Scan(&count)
+
+	for i := 0; i < count; i++ {
+		months := i + 1
+		dpm := capitall * dp
+		mp := dpm * (float64(days))
+		capitall = capitall + mp
+		fmt.Println(months,"ヶ月",int(capitall),"円","利益",int(mp),"円")
+	}
+}
